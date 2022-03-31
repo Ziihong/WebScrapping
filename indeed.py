1df8bae6b7cecb833183d91a2426f747e38a105c
jihong <rlawyd9@kookmin.ac.kr>
2022-03-31 23:12:10 +0900

    FEAT: extract company

--- indeed.py
+++ indeed.py
@@ -25,13 +25,21 @@ def extract_indeed_pages():
 def extract_indeed_jobs(last_page):
     jobs = []
 
-    # 각 페이지에서 직무 title 가져오기
+    # 페이지에서 각 title, company 가져오기
     for page in range(last_page):
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
+
         for job_result in results:
             title = job_result.find("div", {"class": "title"}).find("a")["title"]
+            company = job_result.find("span", {"class": "company"})
+            company_anchor = company.find("a")
+            if company_anchor is not None:
+                company = str(company_anchor.string)
+            else:
+                company = str(company.string)
+            company = company.strip()         
 
     return jobs
 
